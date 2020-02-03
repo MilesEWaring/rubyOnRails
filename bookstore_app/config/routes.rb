81db66319eb1312c35679a33d14a6ef798cb51bf
MilesEWaring <b6029387@my.shu.ac.uk>
2020-02-03 16:26:44 +0000

    Connected to database

--- bookstore_app/config/routes.rb
+++ bookstore_app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/Home'
+  root 'static_pages#Home'
   get 'static_pages/About'
   get 'static_pages/Contact'
+  resources :books
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
